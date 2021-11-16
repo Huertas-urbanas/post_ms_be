c3fabdf24b0f9d50e87cc7f4515a0f99c16c5418
damarishernandez <cortezhernandezdamaris@gmail.com>
2021-11-16 11:31:47 -0500

    Correction null field

--- postApp/models/post.py
+++ postApp/models/post.py
@@ -4,7 +4,7 @@ class Post(models.Model):
     id = models.AutoField(primary_key=True)
     user = models.IntegerField(null=True)
     text = models.TextField()
-    date = models.DateTimeField(auto_now_add=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
     image =  models.URLField(blank=True)
     #category = 
     def datepublished(self): return self.date.strftime('%d %b %Y' )
