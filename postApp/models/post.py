07d444c7102c6f2f47c42f2657e0339cceab0706
damarishernandez <cortezhernandezdamaris@gmail.com>
2021-11-16 14:25:36 -0500

    remove dependencies from user to post

--- postApp/models/post.py
+++ postApp/models/post.py
@@ -2,7 +2,6 @@ from django.db import models
 
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
-    user = models.IntegerField(null=True)
     text = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     image =  models.URLField(blank=True)
--- postApp/serializers/postSerializer.py
+++ postApp/serializers/postSerializer.py
@@ -5,14 +5,13 @@ from rest_framework import serializers
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ['id','user', 'text', 'datepublished','image']
+        fields = ['id', 'text', 'datepublished','image']
 
 def to_representation(self, obj):
     post = Post.objects.get(id=obj.id)
 
     return{
         'id' : obj.id,
-        'user' : obj.user,
         'text' : obj.text,
         'image' : obj.image,
     }
