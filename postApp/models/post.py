d18cd8b5f551933da6163ffc1591595ba6a8bb08
damarishernandez <cortezhernandezdamaris@gmail.com>
2021-11-16 17:06:40 -0500

    Reset changes

--- postApp/models/post.py
+++ postApp/models/post.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Post(models.Model):
-    id = models.AutoField(primary_key=True)
-    text = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
-    image =  models.URLField(blank=True)
-    #category = 
+    id    = models.AutoField(primary_key=True)
+    user  = models.IntegerField(null=True)
+    text  = models.TextField()
+    date  = models.DateTimeField(auto_now_add=True, null=True)
+    image = models.URLField(blank=True)
+
     def datepublished(self): return self.date.strftime('%d %b %Y' )
